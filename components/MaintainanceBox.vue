f76885b820513552bea69403f851ec4cd2683162
Rocket-Fish <rocket.fish@mail.com>
2019-06-10 02:59:14 -0400

    added maintainance box to denote work in progress

--- components/MaintainanceBox.vue
+++ components/MaintainanceBox.vue
@@ -1,3 +1,11 @@
-<template> </template>
+<template>
+  <b-container class="bg-gray">
+    <div class="font-weight-bold">
+      This site is currently undergoing maintainance, certain features may be
+      unavailabe.
+    </div>
+    <div class="font-weight-bold">AdonisJS + Nuxt.js = 🔥</div>
+  </b-container>
+</template>
 <script></script>
 <style scoped></style>
