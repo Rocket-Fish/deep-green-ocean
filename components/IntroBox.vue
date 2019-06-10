ff4b1cd754490624bd16fea9c157492d832efb9d
Rocket-Fish <rocket.fish@mail.com>
2019-06-09 20:36:57 -0400

    created the project structure layout for the index page, index box, maintainance box and the general wrapper for background stuffs.

---
+++ components/IntroBox.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class=" pad_top ">
+    <b-container>
+      <b-row>
+        <b-col sm="4"><ProfilePic /></b-col>
+        <b-col sm="8" class="white_bg">
+          <div>
+            some other stuffs here
+          </div>
+        </b-col>
+      </b-row>
+    </b-container>
+  </section>
+</template>
+<script>
+import ProfilePic from '~/components/ProfilePic.vue'
+
+export default {
+  components: {
+    ProfilePic
+  }
+}
+</script>
+<style lang="scss" scoped>
+.pad_top {
+  padding-top: 10em;
+}
+.white_bg {
+  background-color: white;
+}
+</style>
---
+++ components/ProfilePic.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="image_cropper">
+    <img src="~/assets/profile.png" class="profile_pic" />
+  </div>
+</template>
+<script></script>
+<style lang="scss" scoped>
+.image_cropper {
+  width: 15rem;
+  height: 15rem;
+  position: relative;
+  overflow: hidden;
+  border-radius: 50%;
+}
+
+.profile_pic {
+  display: inline;
+  margin: 0 auto;
+  //margin-left: 1%; //centers the image
+  height: 100%;
+  width: 100%;
+  background-color: #d0d0d0;
+}
+</style>
