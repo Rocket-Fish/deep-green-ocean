83b1a85b06eb97c285626c0c227e08e5e43cb654
Rocket-Fish <rocket.fish@mail.com>
2019-06-11 01:16:12 -0400

    refactored projects.vue to accept dynamic routes. added the ability to see details of a project

---
+++ pages/projects/_id.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    nothing here
+    {{ getFullDescription }}
+  </div>
+</template>
+<script>
+export default {
+  validate({ params }) {
+    // Must be a number
+    return /^\d+$/.test(params.id)
+  },
+  computed: {
+    getFullDescription() {
+      return JSON.stringify(this.$store.state.projects.idToFullDescription)
+    }
+  },
+  async fetch({ store, params }) {
+    await store.dispatch('projects/getFullDescriptionConditionally', params.id)
+  }
+}
+</script>
+<style lang="scss" scoped>
+@import '~assets/constants.scss';
+</style>
--- store/projects.js
+++ store/projects.js
@@ -1,9 +1,20 @@
 export const state = () => ({
-  list: []
+  list: [],
+  // id : {description, links, etc.}
+  idToFullDescription: {}
 })
 
 export const mutations = {
   list(state, data) {
     state.list = data
+  }
+}
+
+export const actions = {
+  async getFullDescriptionConditionally({ commit, state, rootState }, id) {
+    if (typeof state.idToFullDescription[id] === 'undefined') {
+      const data = await this.$axios.$get(`${rootState.api.url}/projects/${id}`)
+      state.idToFullDescription[id] = data
+    }
   }
 }
